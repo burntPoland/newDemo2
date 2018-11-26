305690fcded05a447fb26805a8063afc23e05787
BurntPoland <burntpoland@gmail.com>
2018-11-26 15:39:01 +0100

    City Class added

---
+++ src/main/java/Pl/Demo2/City.java
@@ -0,0 +1,29 @@
+package Pl.Demo2;
+
+public class City {
+	
+	private String cityName;
+	private String postCode;
+	private int zipCode;
+	
+	public String getCityName() {
+		return cityName;
+	}
+	public void setCityName(String cityName) {
+		this.cityName= cityName;
+	}
+	public String getPostCode() {
+		return postCode;
+	}
+	public void setPostCode(String postCode) {
+		this.postCode= postCode;
+	}
+	public int getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(int zipCode) {
+		this.zipCode= zipCode;
+	}
+	
+
+}
