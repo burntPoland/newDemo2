9f65e8600765587161c3639326ca576988ec7b25
BurntPoland <burntpoland@gmail.com>
2018-11-26 15:27:32 +0100

    first commit - class Client added

---
+++ src/main/java/Pl/Demo2/Client.java
@@ -0,0 +1,27 @@
+package Pl.Demo2;
+
+public class Client {
+	
+	private String name;
+	private String lastName;
+	
+	
+	public Client() {
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name=name;
+	}
+	public String getlastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName=lastName;
+	}
+
+}
---
+++ src/main/java/Pl/Demo2/Main.java
@@ -0,0 +1,19 @@
+package Pl.Demo2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		Client client1 = new Client();
+		
+		client1.setName("Michael");
+		
+		client1.setLastName("Scott");
+		
+		System.out.println(client1.getName() + " " + client1.getlastName());
+		
+		
+	}
+
+}
